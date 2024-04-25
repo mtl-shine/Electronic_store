6122c6c2c9cad11ba79cc65da78c4bf1012ba5d2
Tatiana Nikitina <e.mtl.shine@gmail.com>
2024-04-25 23:26:47 +0200

    Fields description was done

---
+++ Laptop.java
@@ -0,0 +1,19 @@
+// Подумать над структурой класса Ноутбук для магазина техники - выделить поля и
+// методы. Реализовать в java.
+
+public class Laptop {
+    private String brand;
+    private String model;
+    private String buildCode;
+    private int ram;
+    private int storage;
+    private String processor;
+    private String graphCard;
+    private String operatingSys;
+    private String dispDiagonal;
+    private String battary;
+    private String color;
+    private int weight;
+    private int quantity;
+    private int price;
+}
